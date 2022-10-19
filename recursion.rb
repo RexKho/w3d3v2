6eafa922feb66be35142279ef23bb4e95e7186b4
AA Student <appacademy@Mini-21.local>
2022-10-19 16:40:38 -0700

    finished upto permutations

--- recursion.rb
+++ recursion.rb
@@ -173,5 +173,19 @@ end
 # p subsets([1, 2]) # => [[], [1], [2], [1, 2]]
 # p subsets([1, 2, 3])
 # # => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
-p subsets([1,2,3,4])
+# p subsets([1,2,3,4])
+
+def permutations(arr)
+    return [arr[0]] if arr.length == 1
+    return [[arr[0], arr[1]], [arr[1], arr[0]]] if arr.length == 2
+    new_arr = []
+    (0...arr.length).each do |i|
+        new_arr += permutations(arr[0...i]+arr[(i+1)..-1]).map { |el| el += [arr[i]] }
+    end
+    new_arr
+end
+
+# p permutations([1, 2, 3, 4, 5]).length # => [[1, 2, 3], [1, 3, 2],
+#                         #     [2, 1, 3], [2, 3, 1],
+#                         #     [3, 1, 2], [3, 2, 1]]
 
